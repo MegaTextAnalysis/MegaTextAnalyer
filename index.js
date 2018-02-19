a7d8e221e0b4a8187b7b37f5def9ae91be789591
Hieu Nguyen <contact@nguyenhi.eu>
2018-02-19 17:56:46 +0000

    Add text flagging

--- index.js
+++ index.js
@@ -3,11 +3,9 @@
 const Twitter = require("twitter");
 const Express = require("express");
 const Credentials = require("./credentials_twitter");
-//let array = ["Isis","Allahu Akbar","Caliphate","Isil","Islamic State"];
-//let array2 = ["Brothers","Cut","Death"];
-let array = ["Ireland","Irish","6 Nations","#IREvWAL"];
-let flaggedTweets = [];
-let count = 0;
+// let array = ["Isis","Allahu Akbar","Caliphate","Isil","Islamic State"];
+// let array2 = ["Brothers","Cut","Death"];
+let array = ["Ireland", "Irish", "6 Nations", "#IREvWAL"];
 
 let client = new Twitter({
   consumer_key: Credentials.CONSUMER_KEY,
@@ -26,36 +24,27 @@ server.get("/", function(req, res) {
 
 // Handles username fetching
 server.get("/:handle", function(req, res) {
+  let flaggedTweets = [];
+
   getTweets(req.params.handle, function(tweets) {
-  for (let i = 0; i < array.length; i++) {
-    for (let x = 0; x <= 14; x++) {
-      if (tweets[x].text.includes(array[i])) {
-        flaggedTweets[count]=tweets[x].text;
-        count++;
+    for (let i of tweets) {
+      for (let j of array) {
+        if (i.text.indexOf(j) > -1) {
+          console.log(i.text);
+          flaggedTweets.push(j);
+        }
       }
     }
-  }
-  //for (let i = 0; i < array2.length; i++) {
-    for (let x = 0; x < flaggedTweets.length; x++) {
-      //if(flaggedTweets[x].includes(array2[i]))
-      //{
-        console.log(flaggedTweets[x]);
-        //count++;
-      //}
-    }
-  //}
 
-    console.log(count + " Keywords Recognised.");
-    res.json(tweets);
+    console.log(flaggedTweets);
+    res.send("OK");
   });
 });
-
 
 // Handles Twitter API querying
 function getTweets(handle, callback) {
   client.get("statuses/user_timeline", {
     screen_name: handle,
-    exclude_replies: 1,
     include_rts: 0,
     trim_user: 1
   }, function(err, tweets, res) {
