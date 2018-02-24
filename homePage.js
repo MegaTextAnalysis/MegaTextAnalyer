a797da8d4490b6bbf3f8d8763bea684244b39389
stephenMoran <morans8@tcd.ie>
2018-02-24 16:19:36 +0000

    backend updates

---
+++ homePage.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    showhide();
+});
+
+$("#btnSubmit").click(function () {
+    var word = document.getElementById("search");
+    var url = " radical.nguyenhi.eu/" + word.value;
+    console.log("2");
+    $.get(url, function (data) { console.log(data)});
+});
+
+$('#another-location').keypress(function (event) {
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+        alert('You pressed a "enter" key in textbox, here submit your form');
+    }
+});
+
+$("#s1").click(function () {
+    showhide();
+});
+
+function showhide() {
+    var e = document.getElementById("sel-1");
+    var numDiv = document.getElementById("s2");
+    var  searchType = e.options[e.selectedIndex].text;
+    console.log(searchType);
+    if (searchType == "Username") {
+        numDiv.style.display = "none";
+        console.log("hi");
+    }
+    else {
+        numDiv.style.display = "block";
+    }
+}
