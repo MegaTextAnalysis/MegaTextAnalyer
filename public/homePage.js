15a3464e6169cc7b25f6f6da1cb3aeac5748407f
stephenMoran <morans8@tcd.ie>
2018-02-25 16:33:26 +0000

    fix

--- public/homePage.js
+++ public/homePage.js
@@ -3,30 +3,30 @@ $(document).ready(function () {
 
 
     $("#btnSubmit").click(function () {
-    var word = document.getElementById("search");
-    var url = "/" + word.value;
-    console.log("2");
-    $.get(url, function (data) {console.log(33)});
-});
+        var word = document.getElementById("search");
+        var url = "/" + word.value;
+        console.log("2");
+        $.get(url, function (data) { console.log(data) });
+    });
+
+    $('#another-location').keypress(function (event) {
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if (keycode == '13') {
+            alert('You pressed a "enter" key in textbox, here submit your form');
+        }
+    });
+
+    $("#s1").click(function () {
+        showhide();
+    });
 
 });
 
-
-$('#another-location').keypress(function (event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if (keycode == '13') {
-        alert('You pressed a "enter" key in textbox, here submit your form');
-    }
-});
-
-$("#s1").click(function () {
-    showhide();
-});
 
 function showhide() {
     var e = document.getElementById("sel-1");
     var numDiv = document.getElementById("s2");
-    var  searchType = e.options[e.selectedIndex].text;
+    var searchType = e.options[e.selectedIndex].text;
     console.log(searchType);
     if (searchType == "Username") {
         numDiv.style.display = "none";
