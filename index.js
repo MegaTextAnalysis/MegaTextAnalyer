9a6a526212ae1a9d29a7c7e98a51b3e0d6967008
Hieu Nguyen <hieunguyenvn1@gmail.com>
2018-03-01 15:37:57 +0000

    Add flagged info to requests and flagged words

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const Twitter = require("twitter");
 const Express = require("express");
 const Credentials = require("./credentials_twitter");
+const Flags = require("./flags");
 
 let client = new Twitter({
   consumer_key: Credentials.CONSUMER_KEY,
@@ -12,7 +13,8 @@ let client = new Twitter({
 });
 
 const server = Express();
-server.listen(3000);
+server.use(Express.static("public"));
+server.listen(80);
 
 // Handles landing page
 server.get("/", function(req, res) {
@@ -22,15 +24,58 @@ server.get("/", function(req, res) {
 // Handles username fetching
 server.get("/user/:handle", function(req, res) {
   getTweets(req.params.handle, function(tweets) {
-    console.log(tweets);
-    res.json(tweets);
+    let jsonObj = {};
+    jsonObj.flagged = [];
+    let flaggedTweet = {};
+
+    for (let i of tweets) {
+      flaggedTweet.flags = [];
+      flaggedTweet.text = i.text;
+      flaggedTweet.threatLevel = 0;
+
+      for (let j of Flags) {
+        if (i.text.indexOf(j) > -1) {
+          flaggedTweet.flags.push(j);
+        }
+      }
+
+      if (flaggedTweet.flags.length > 0) {
+        jsonObj.flagged.push(flaggedTweet);
+        flaggedTweet = {};
+      }
+    }
+
+    jsonObj.tweets = tweets;
+    res.json(jsonObj);
   });
 });
 
 server.get("/search/:query", function(req, res) {
   search(req.params.query, function(tweets) {
-    console.log(tweets);
-    res.json(tweets);
+    let jsonObj = {};
+    jsonObj.flagged = [];
+    let flaggedTweet = {};
+
+    for (let i of tweets.statuses) {
+      flaggedTweet.flags = [];
+      flaggedTweet.text = i.text;
+      flaggedTweet.threatLevel = 0;
+      flaggedTweet.user = i.user.screen_name;
+
+      for (let j of Flags) {
+        if (i.text.indexOf(j) > -1) {
+          flaggedTweet.flags.push(j);
+        }
+      }
+
+      if (flaggedTweet.flags.length > 0) {
+        jsonObj.flagged.push(flaggedTweet);
+        flaggedTweet = {};
+      }
+    }
+
+    jsonObj.tweets = tweets.statuses;
+    res.json(jsonObj);
   });
 });
 
