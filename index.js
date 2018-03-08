b67568e1cf42f05773cc088132ce42e14ab8c74c
simong21 <35733539+simong21@users.noreply.github.com>
2018-03-08 15:48:46 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -20,11 +20,6 @@ let textapi = new AYLIENTextAPI({
   application_key: aylienCreds.APP_KEY
 });
 
-// Gets lists of keywords and
-// categories for AI
-let keys = Flags.keywords;
-let categories = Categories.categories; 
-
 const server = Express();
 server.use(Express.static("public"));
 server.listen(80);
@@ -37,122 +32,128 @@ server.get("/", function(req, res) {
 // Handles username fetching
 server.get("/user/:handle", function(req, res) {
   getTweets(req.params.handle, function(tweets) {
-    iterateFlagging(tweets, function(jsonObj) {
-      jsonObj.tweets = tweets;
-      res.json(jsonObj);
-      
-    });
+    let jsonObj = {};
+    let promises = [];
+    jsonObj.totalRisk = 0;
+    jsonObj.flagged = [];
+
+    for (let i of tweets) {
+      promises.push(new Promise((resolve, reject) => {
+        let flaggedTweet = {};
+        let text = i.text;
+        flaggedTweet.flags = [];
+        flaggedTweet.text = text;
+        flaggedTweet.threatLevel = 0;
+
+        callAI(text)
+          .then(labels => {
+            flaggedTweet.label = labels;
+
+            Categories.forEach((element) => {
+              if (labels.indexOf(element) > -1) {
+                flaggedTweet.threatLevel += 20;
+              }
+            });
+
+            return flaggedTweet;
+          })
+          .then(flaggedTweet => {
+            for (let j in Flags) {
+              if (text.toUpperCase().indexOf(j.toUpperCase()) > -1) {
+                flaggedTweet.flags.push(j);
+                flaggedTweet.threatLevel += Flags[j];
+              }
+            }
+          })
+          .then(() =>
+            resolve(flaggedTweet))
+          .catch(error => {
+            console.log(error);
+          });
+      }));
+    }
+
+    Promise.all(promises)
+      .then(array => {
+        array.forEach(flagged => {
+          if (flagged.flags.length > 0) {
+            jsonObj.totalRisk+=flagged.threatLevel;
+            jsonObj.flagged.push(flagged);
+          }
+        });
+      })
+      .then(() => {
+        jsonObj.flagged.sort(function(a,b){
+          return b.threatLevel-a.threatLevel;
+        });
+        jsonObj.tweets = tweets;
+        res.json(jsonObj);
+      });
   });
 });
-
-function iterateFlagging(tweets, callback) {
-  let jsonObj = {};
-  jsonObj.totalRisk=0;
-  jsonObj.flagged = [];
-
-  for (let i = 0; i < tweets.length; i++) {
-    flagTweet(tweets[i].text, function(flaggedTweet, isFlagged) {
-      callAI(tweets[i], function(labels) {
-        flaggedTweet.label = labels;
-        for(let j of categories)
-        {
-          if (labels.indexOf(j) > -1) {
-            flaggedTweet.threatLevel+=20;
-          }
-        }
-
-        if (isFlagged) {
-          jsonObj.totalRisk+=flaggedTweet.threatLevel;
-          jsonObj.flagged.push(flaggedTweet);
-          console.log(flaggedTweet);
-        }
-
-        if (i === tweets.length - 1) {
-          callback(jsonObj);
-        }
-      });
-    });
-  }
-}
-
-function flagTweet(tweet, callback) {
-  let flaggedTweet = {};
-  flaggedTweet.flags = [];
-  flaggedTweet.text = tweet;
-  flaggedTweet.threatLevel = 0;
-
-  for (let i in keys) {
-    if (tweet.toUpperCase().indexOf(i.toUpperCase()) > -1) {
-      flaggedTweet.flags.push(i);
-      flaggedTweet.threatLevel += keys[i];
-    }
-  }
-
-  if (flaggedTweet.flags.length > 0) {
-    callback(flaggedTweet, true);
-  } else {
-    callback(flaggedTweet, false);
-  }
-}
 
 server.get("/search/:query", function(req, res) {
   search(req.params.query, function(tweets) {
-    iterateFlaggingSearch(tweets.statuses, function(jsonObj) {
-      jsonObj.tweets = tweets;
-      res.json(jsonObj);
-    });
+    let jsonObj = {};
+    let promises = [];
+    jsonObj.totalRisk = 0;
+    jsonObj.flagged = [];
+
+    for(let i of tweets.statuses){
+      promises.push(new Promise((resolve, reject) => {
+        let flaggedTweet = {};
+        let text = i.text;
+        flaggedTweet.flags = [];
+        flaggedTweet.text = text;
+        flaggedTweet.threatLevel = 0;
+    
+        callAI(text)
+          .then(labels => {
+            flaggedTweet.label = labels;
+
+            Categories.forEach((element) => {
+              if (labels.indexOf(element) > -1) {
+                flaggedTweet.threatLevel += 20;
+              }
+            });
+
+            return flaggedTweet;
+          })
+          .then(flaggedTweet => {
+            for (let j in Flags) {
+              if (text.toUpperCase().indexOf(j.toUpperCase()) > -1) {
+                flaggedTweet.flags.push(j);
+                flaggedTweet.threatLevel += Flags[j];
+              }
+            }
+          })
+          .then(() =>
+            resolve(flaggedTweet))
+          .catch(error => {
+            console.log(error);
+          });
+      }));
+    }
+
+    Promise.all(promises)
+      .then(array => {
+        array.forEach(flagged => {
+          if (flagged.flags.length > 0) {
+            jsonObj.totalRisk+=flagged.threatLevel;
+            jsonObj.flagged.push(flagged);
+          }
+        });
+      })
+      .then(() => {
+        jsonObj.flagged.sort(function(a,b){
+          return b.threatLevel-a.threatLevel;
+        });
+        jsonObj.tweets = tweets;
+        res.json(jsonObj);
+      });
   });
 });
 
-  function iterateFlaggingSearch(tweets, callback) {
-  let jsonObj = {};
-  jsonObj.flagged = [];
-
-  for (let i = 0; i < tweets.length; i++) {
-    flagTweetSearch(tweets[i], function(flaggedTweet, isFlagged) {
-      callAI(tweets[i], function(labels) {
-        flaggedTweet.label = labels;
-
-        for(let j of categories)
-        {
-          if (labels.indexOf(j) > -1) {
-            flaggedTweet.threatLevel+=20;
-          }
-        }
-
-        if (isFlagged) {
-          jsonObj.flagged.push(flaggedTweet);
-          console.log(flaggedTweet);
-        }
-
-        if (i === tweets.length - 1) {
-          callback(jsonObj);
-        }
-      });
-    });
-  }
-}
-
-function flagTweetSearch(tweet, callback) {
-  let flaggedTweet = {};
-  flaggedTweet.flags = [];
-  flaggedTweet.text = tweet.text;
-  flaggedTweet.threatLevel = 0;
-  flaggedTweet.user = tweet.user.screen_name;
-
-  for (let i in keys) {
-    if (tweet.text.toUpperCase().indexOf(i.toUpperCase()) > -1) {
-      flaggedTweet.flags.push(i);
-      flaggedTweet.threatLevel += keys[i];
-    }
-  }
-
-  if (flaggedTweet.flags.length > 0) {
-    callback(flaggedTweet, true);
-  } else {
-    callback(flaggedTweet, false);
-  }
-}
 
 // Handles timeline querying
 function getTweets(handle, callback) {
@@ -183,17 +184,19 @@ function search(query, callback) {
   });
 }
 
-function callAI(i, callback) {
-  textapi.classifyByTaxonomy({
-    "text": i.text,
-    "taxonomy": "iptc-subjectcode"
-  }, function(error, response) {
-    if (!error) {
-      let x = response["categories"];
-      let labels = x[0].label;
-      callback(labels);
-    } else {
-      console.log(error);
-    }
+function callAI(i) {
+  return new Promise(function(resolve, reject) {
+    textapi.classifyByTaxonomy({
+      "text": i,
+      "taxonomy": "iptc-subjectcode"
+    }, function(error, response) {
+      if (!error) {
+        let x = response["categories"];
+        let labels = x[0].label;
+        resolve(labels);
+      } else {
+        reject(new Error("Call AI failed."));
+      }
+    });
   });
 }
