eee760f4cd426a717b615bd334356f6e7b1f00dd
stephenMoran <morans8@tcd.ie>
2018-03-16 12:06:53 +0000

    frontend fix

--- credentials_aylien.js
+++ credentials_aylien.js
@@ -3,4 +3,6 @@ module.exports = {
   APP_KEY: "684197d60bb37b3b36e16a0bb5757083"
   //APP_ID: "7cc0ba58",
   //APP_KEY: "7b559363068eec01482ecd96073fe4fd"
+  //App_ID: 2c2d73f7
+  //APP_KEY: 51407d61ac48294ef39969b2f8329ba3
 };
