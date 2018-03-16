6652f958be7ded523f77184f7139ff4d8ad9edab
Hieu Nguyen <hieunguyenvn1@gmail.com>
2018-03-16 11:04:14 +0000

    Revert "Add Promise function to AnalysisPromise"
    This reverts commit 9c8751f2e3c67f8cbadf55811c2cdac465cdbce4.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Express = require("express");
 const Datastore = require("nedb");
 const AnalysisPromise = require("./AnalysisPromise");
 const TwitterUtils = require("./TwitterUtils");
-const AnalysisUtils = require("./AnalysisJSON");
+const AnalysisJSON = require("./AnalysisJSON");
 
 let db = new Datastore({
   filename: "./database",
@@ -23,17 +23,39 @@ server.listen(80);
 // Handles username fetching
 server.get("/user/:handle", (req, res) => {
   twitter.getTweets(req.params.handle, (tweets) => {
-    let jsonObj = new AnalysisUtils();
+    let jsonObj = new AnalysisJSON();
 
     // Create array of Promises of analyses of tweets
     let promises = tweets.map(entries => analysis.analyse(entries.text));
 
-    jsonObj = analysis.promiseAll(jsonObj, promises, tweets);
+    // Wait for Promises to finish and get total threat level
+    Promise.all(promises)
+      .then(array => {
+        array.forEach(flagged => {
+          if (flagged.flags.length > 0) {
+            jsonObj.totalRisk += flagged.threatLevel;
+            jsonObj.flagged.push(flagged);
+          }
+        });
+      })
+      .then(() => {
+        // Sort flagged tweets by threat level
+        jsonObj.flagged.sort((a, b) => {
+          return b.threatLevel - a.threatLevel;
+        });
 
-    // Check and update database with new results
-    checkDB(tweets, req.params.handle, jsonObj);
+        // Set to risk to 100 if over 100
+        if (jsonObj.totalRisk > 100) {
+          jsonObj.totalRisk = 100;
+        }
 
-    res.json(jsonObj);
+        jsonObj.tweets = tweets;
+
+        // Check and update database with new results
+        checkDB(tweets, req.params.handle, jsonObj);
+
+        res.json(jsonObj);
+      });
   });
 });
 
@@ -51,13 +73,35 @@ server.get("/db", (req, res) => {
 // Handles search querying
 server.get("/search/:query", function(req, res) {
   twitter.search(req.params.query, function(tweets) {
-    let jsonObj = new AnalysisUtils();
+    let jsonObj = new AnalysisJSON();
 
     // Iterate over tweets and analyse
     let promises = tweets.statuses.map(entries => analysis.analyse(entries.text));
 
-    // Wait for Promises to finish apply analyses to JSON
-    res.json(analysis.promiseAll(jsonObj, promises, tweets));
+    // Wait for Promises to finish and get total threat level
+    Promise.all(promises)
+      .then(array => {
+        array.forEach(flagged => {
+          if (flagged.flags.length > 0) {
+            jsonObj.totalRisk += flagged.threatLevel;
+            jsonObj.flagged.push(flagged);
+          }
+        });
+      })
+      .then(() => {
+        // Sort flagged tweets by threat level
+        jsonObj.flagged.sort(function(a, b) {
+          return b.threatLevel - a.threatLevel;
+        });
+
+        // Set to risk to 100 if over 100
+        if (jsonObj.totalRisk > 100) {
+          jsonObj.totalRisk = 100;
+        }
+
+        jsonObj.tweets = tweets;
+        res.json(jsonObj);
+      });
   });
 });
 
