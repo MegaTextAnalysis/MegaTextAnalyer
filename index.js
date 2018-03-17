9f56592e8b070042b0a2526860b8a8474fae3eb9
Hieu Nguyen <hieunguyenvn1@gmail.com>
2018-03-17 21:29:02 +0000

    Update codebase

--- index.js
+++ index.js
@@ -1,202 +1,134 @@
 "use strict";
 
-const Twitter = require("twitter");
+// Require modules
 const Express = require("express");
-const Credentials = require("./credentials_twitter");
-const AYLIENTextAPI = require("aylien_textapi");
-const aylienCreds = require("./credentials_aylien");
-const Flags = require("./flags");
-const Categories = require("./categories");
+const Datastore = require("nedb");
+const AnalysisPromise = require("./AnalysisPromise");
+const TwitterUtils = require("./TwitterUtils");
+const AnalysisJSON = require("./AnalysisJSON");
 
-let client = new Twitter({
-  consumer_key: Credentials.CONSUMER_KEY,
-  consumer_secret: Credentials.CONSUMER_SECRET,
-  access_token_key: Credentials.ACCESS_TOKEN,
-  access_token_secret: Credentials.ACCESS_SECRET
+let db = new Datastore({
+  filename: "./database",
+  autoload: true
 });
 
-let textapi = new AYLIENTextAPI({
-  application_id: aylienCreds.APP_ID,
-  application_key: aylienCreds.APP_KEY
-});
+let twitter = new TwitterUtils();
+let analysis = new AnalysisPromise();
 
+// Express server
 const server = Express();
 server.use(Express.static("public"));
 server.listen(80);
 
-// Handles landing page
-server.get("/", function(req, res) {
-  res.send("Application running.");
-});
-
 // Handles username fetching
-server.get("/user/:handle", function(req, res) {
-  getTweets(req.params.handle, function(tweets) {
-    let jsonObj = {};
-    let promises = [];
-    jsonObj.totalRisk = 0;
-    jsonObj.flagged = [];
+server.get("/user/:handle", (req, res) => {
+  twitter.getTweets(req.params.handle, (tweets) => {
+    let jsonObj = new AnalysisJSON();
 
-    for (let i of tweets) {
-      promises.push(new Promise((resolve, reject) => {
-        let flaggedTweet = {};
-        let text = i.text;
-        flaggedTweet.flags = [];
-        flaggedTweet.text = text;
-        flaggedTweet.threatLevel = 0;
+    // Create array of Promises of analyses of tweets
+    let promises = tweets.map(entries => analysis.analyse(entries.text));
 
-        callAI(text)
-          .then(labels => {
-            flaggedTweet.label = labels;
-
-            Categories.forEach((element) => {
-              if (labels.indexOf(element) > -1) {
-                flaggedTweet.threatLevel += 20;
-              }
-            });
-
-            return flaggedTweet;
-          })
-          .then(flaggedTweet => {
-            for (let j in Flags) {
-              if (text.toUpperCase().indexOf(j.toUpperCase()) > -1) {
-                flaggedTweet.flags.push(j);
-                flaggedTweet.threatLevel += Flags[j];
-              }
-            }
-          })
-          .then(() =>
-            resolve(flaggedTweet))
-          .catch(error => {
-            console.log(error);
-          });
-      }));
-    }
-
+    // Wait for Promises to finish and get total threat level
     Promise.all(promises)
       .then(array => {
         array.forEach(flagged => {
           if (flagged.flags.length > 0) {
-            jsonObj.totalRisk+=flagged.threatLevel;
+            jsonObj.totalRisk += flagged.threatLevel;
             jsonObj.flagged.push(flagged);
           }
         });
       })
       .then(() => {
-        jsonObj.flagged.sort(function(a,b){
-          return b.threatLevel-a.threatLevel;
+        // Sort flagged tweets by threat level
+        jsonObj.flagged.sort((a, b) => {
+          return b.threatLevel - a.threatLevel;
         });
+
+        // Set to risk to 100 if over 100
+        if (jsonObj.totalRisk > 100) {
+          jsonObj.totalRisk = 100;
+        }
+
         jsonObj.tweets = tweets;
+
+        // Check and update database with new results
+        checkDB(tweets, req.params.handle, jsonObj);
+
         res.json(jsonObj);
       });
   });
 });
 
-server.get("/search/:query", function(req, res) {
-  search(req.params.query, function(tweets) {
-    let jsonObj = {};
-    let promises = [];
-    jsonObj.totalRisk = 0;
-    jsonObj.flagged = [];
-
-    for(let i of tweets.statuses){
-      promises.push(new Promise((resolve, reject) => {
-        let flaggedTweet = {};
-        let text = i.text;
-        flaggedTweet.flags = [];
-        flaggedTweet.text = text;
-        flaggedTweet.threatLevel = 0;
-    
-        callAI(text)
-          .then(labels => {
-            flaggedTweet.label = labels;
-
-            Categories.forEach((element) => {
-              if (labels.indexOf(element) > -1) {
-                flaggedTweet.threatLevel += 20;
-              }
-            });
-
-            return flaggedTweet;
-          })
-          .then(flaggedTweet => {
-            for (let j in Flags) {
-              if (text.toUpperCase().indexOf(j.toUpperCase()) > -1) {
-                flaggedTweet.flags.push(j);
-                flaggedTweet.threatLevel += Flags[j];
-              }
-            }
-          })
-          .then(() =>
-            resolve(flaggedTweet))
-          .catch(error => {
-            console.log(error);
-          });
-      }));
-    }
-
-    Promise.all(promises)
-      .then(array => {
-        array.forEach(flagged => {
-          if (flagged.flags.length > 0) {
-            jsonObj.totalRisk+=flagged.threatLevel;
-            jsonObj.flagged.push(flagged);
-          }
-        });
-      })
-      .then(() => {
-        jsonObj.flagged.sort(function(a,b){
-          return b.threatLevel-a.threatLevel;
-        });
-        jsonObj.tweets = tweets;
-        res.json(jsonObj);
-      });
-  });
-});
-
-
-// Handles timeline querying
-function getTweets(handle, callback) {
-  client.get("statuses/user_timeline", {
-    screen_name: handle,
-    exclude_replies: 1,
-    include_rts: 0,
-    trim_user: 1
-  }, function(err, tweets, res) {
+// Get database of results
+server.get("/db", (req, res) => {
+  db.find({}, (err, docs) => {
     if (err) {
       console.log(err);
     } else {
-      callback(tweets);
+      res.json(docs);
     }
   });
-}
+});
 
 // Handles search querying
-function search(query, callback) {
-  client.get("search/tweets", {
-    q: query
-  }, function(err, tweets, res) {
-    if (err) {
-      console.log(err);
-    } else {
-      callback(tweets);
-    }
-  });
-}
+server.get("/search/:query", function(req, res) {
+  twitter.search(req.params.query, function(tweets) {
+    let jsonObj = new AnalysisJSON();
 
-function callAI(i) {
-  return new Promise(function(resolve, reject) {
-    textapi.classifyByTaxonomy({
-      "text": i,
-      "taxonomy": "iptc-subjectcode"
-    }, function(error, response) {
-      if (!error) {
-        let x = response["categories"];
-        let labels = x[0].label;
-        resolve(labels);
+    // Iterate over tweets and analyse
+    let promises = tweets.statuses.map(entries => analysis.analyse(entries.text));
+
+    // Wait for Promises to finish and get total threat level
+    Promise.all(promises)
+      .then(array => {
+        array.forEach(flagged => {
+          if (flagged.flags.length > 0) {
+            jsonObj.totalRisk += flagged.threatLevel;
+            jsonObj.flagged.push(flagged);
+          }
+        });
+      })
+      .then(() => {
+        // Sort flagged tweets by threat level
+        jsonObj.flagged.sort(function(a, b) {
+          return b.threatLevel - a.threatLevel;
+        });
+
+        // Set to risk to 100 if over 100
+        if (jsonObj.totalRisk > 100) {
+          jsonObj.totalRisk = 100;
+        }
+
+        jsonObj.tweets = tweets;
+        res.json(jsonObj);
+      });
+  });
+});
+
+function checkDB(tweets, handle, jsonObj) {
+  if (tweets.length > 0) {
+    db.find({
+      username: handle
+    }, (err, docs) => {
+      if (err) {
+        console.log(err);
+      } else if (docs.length > 0) {
+        // If exists, update
+        db.update({
+          username: handle
+        }, {
+          $set: {
+            time: Date.now(),
+            data: jsonObj
+          }
+        });
       } else {
-        reject(new Error("Call AI failed."));
+        db.insert({
+          username: handle,
+          time: Date.now(),
+          data: jsonObj
+        });
       }
     });
-  });
+  }
 }
--- public/homePage.js
+++ public/homePage.js
@@ -1,176 +1,158 @@
-$(document).ready(function () {
+$(document).ready(function() {
+  showhide();
+
+  // When user clicks search
+  $("#btnSubmit").click(function() {
+    var word = document.getElementById("search");
+    if (usernameSet()) {
+      getUser(word);
+    } else {
+      getKeyword(word);
+    }
+  });
+
+  /*
+  $("#search").keypress(function (event) {
+      var word = document.getElementById("search");
+      if (usernameSet()) {
+          var url = "/user/" + word.value;
+          $.get(url, function (data) {
+              var obj = data;
+              parseUser(obj);
+          });
+      }
+      else {
+          var url = "/search/" + word.value;
+          $.get(url, function (data) {
+              var obj = data;
+              parseKeyword(obj);
+          });
+      }
+  });
+  */
+
+  // only display number of results selector when the user is searching for keywords
+  $("#s1").click(function() {
     showhide();
-
-    //When user clicks search
-    $("#btnSubmit").click(function () {
-        var word = document.getElementById("search");
-        if (usernameSet()) {
-            getUser(word);
-        }
-        else {
-            getKeyword(word);
-        }
-    });
-
-
-
-    /*
-    $('#search').keypress(function (event) {
-        var word = document.getElementById("search");
-        if (usernameSet()) {
-            var url = "/user/" + word.value;
-            $.get(url, function (data) {
-                var obj = data;
-                parseUser(obj);
-            });
-        }
-        else {
-            var url = "/search/" + word.value;
-            $.get(url, function (data) {
-                var obj = data;
-                parseKeyword(obj);
-            });
-        }
-    });
-    */
-
-    //only display number of results selector when the user is searching for keywords
-    $("#s1").click(function () {
-        showhide();
-    });
-
+  });
 });
 
-
 function getUser(word) {
-    var url = "/user/" + word.value;
-    $.get(url, function (data) {
-        var obj = data;
-        parseUser(obj, word.value);
-    });
+  var url = "/user/" + word.value;
+  $.get(url, function(data) {
+    var obj = data;
+    parseUser(obj, word.value);
+  });
 }
 
 function getKeyword(word) {
-    var url = "/search/" + word.value;
-    $.get(url, function (data) {
-        var obj = data;
-        parseKeyword(obj);
-    });
+  var url = "/search/" + word.value;
+  $.get(url, function(data) {
+    var obj = data;
+    parseKeyword(obj);
+  });
 }
 
-//returns if selector is set to username
+// returns if selector is set to username
 function usernameSet() {
-    var e = document.getElementById("sel-1");
-    var searchType = e.options[e.selectedIndex].text;
-    if (searchType == "Username") {
-        return true;
-    } else {
-        return false;
-    }
+  var e = document.getElementById("sel-1");
+  var searchType = e.options[e.selectedIndex].text;
+  if (searchType === "Username") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-//toggles the number of results selector
+// toggles the number of results selector
 function showhide() {
-
-    var numDiv = document.getElementById("s2");
-    if (usernameSet()) {
-        numDiv.style.display = "none";
-        console.log("hi");
-    } else {
-        numDiv.style.display = "block";
-    }
+  var numDiv = document.getElementById("s2");
+  if (usernameSet()) {
+    numDiv.style.display = "none";
+    console.log("hi");
+  } else {
+    numDiv.style.display = "block";
+  }
 }
 
-
-//parse the incoming JSON 
+// parse the incoming JSON
 function parseJSON(obj) {
-    console.log(obj.flagged)
+  console.log(obj.flagged);
 }
 
-//parse the incoming JSON
+// parse the incoming JSON
 function parseUser(obj, username) {
-    document.getElementById("m1").style.display = 'none';
-    document.getElementById("sb").style.display = 'none';
+  document.getElementById("m1").style.display = "none";
+  document.getElementById("sb").style.display = "none";
 
-    //construct results table
-    var txt = "";
-    console.log(obj);
-    var row = 1;
+  // construct results table
+  var txt = "";
+  console.log(obj);
+  var row = 1;
 
-    if (obj.flagged.length !== 0) {
-        for (x in obj.flagged) {
-            txt += "<tr>" +
-                "<th scope='row'>" + row + "</th>" +
-                "<td>" + obj.flagged[x].text + "</td>" +
-                "</tr>";
-            row++;
-        }
+  if (obj.flagged.length !== 0) {
+    for (let x in obj.flagged) {
+      txt += "<tr>" +
+        "<th scope=" + row + ">" + row + "</th>" + "<td>" + obj.flagged[x].text + "</td>" + "</tr>";
+      row++;
     }
-    else {
-        txt += "<tr>" +
-            "<th scope='row'></th>" +
-            "<td>No tweets on this users profile have been flagged</td>" +
-            "</tr>";
-    }
+  } else {
+    txt += "<tr>" + "<th scope=" + row + "></th>" + "<td>No tweets on this users profile have been flagged</td>" + "</tr>";
+  }
 
-    //popuulate table and display
-    document.getElementById("body").innerHTML = txt;
-    document.getElementById("userResults").style.display = 'block';
+  // populate table and display
+  document.getElementById("body").innerHTML = txt;
+  document.getElementById("userResults").style.display = "block";
 
-    console.log(txt);
-    //set threat meter
-    document.getElementById("threat").style.width = obj.totalRisk + "%";
-    document.getElementById("threat").innerHTML = obj.totalRisk;
+  console.log(txt);
+  // set threat meter
+  document.getElementById("threat").style.width = obj.totalRisk + "%";
+  document.getElementById("threat").innerHTML = obj.totalRisk;
 
-    //set image
-    document.getElementById("img1").src = "https://twitter.com/" + username + "/profile_image?size=original";
+  // set image
+  document.getElementById("img1").src = "https://twitter.com/" + username + "/profile_image?size=original";
 
-    //set link to profile
-    document.getElementById("link").href = "https://twitter.com/" + username + "?lang=en";
-
-
-
+  // set link to profile
+  document.getElementById("link").href = "https://twitter.com/" + username + "?lang=en";
+  document.getElementById("username").innerText = "Username: " + username;
 }
 
 function backToHomepage() {
-    document.getElementById("userResults").style.display = 'none';
-    document.getElementById("keyResults").style.display = 'none';
-    document.getElementById("m1").style.display = 'block';
-    document.getElementById("sb").style.display = 'block';
+  document.getElementById("userResults").style.display = "none";
+  document.getElementById("keyResults").style.display = "none";
+  document.getElementById("m1").style.display = "block";
+  document.getElementById("sb").style.display = "block";
 }
 
 function parseKeyword(obj) {
-    document.getElementById("m1").style.display = 'none';
-    document.getElementById("sb").style.display = 'none';
+  document.getElementById("m1").style.display = "none";
+  document.getElementById("sb").style.display = "none";
 
-    //construct results table
-    var txt = "";
-    console.log(obj);
-    var row = 1;
+  // construct results table
+  var txt = "";
+  console.log(obj);
+  var row = 1;
 
-    if (obj.tweets.statuses.length !== 0) {
-        for (x in obj.tweets.statuses) {
-
-            txt += "<tr>" +
-                "<th scope='row'>" + row + "</th>" +
-                "<td id = 't" + row + "' + >" + "<a onclick = 'getUser(document.getElementById('t" + row + "').innerHTML)'>" + obj.tweets.statuses[row - 1].user.name + "</a></td>" +
-                "<div class='tooltip'>" +
-                "<td>" + obj.tweets.statuses[x].text + "</td>" +
-                " <span class='tooltiptext'>Tooltip text</span></div>" +
-                "</tr>";
-            row++;
-        }
+  if (obj.tweets.statuses.length !== 0) {
+    for (let x in obj.tweets.statuses) {
+      txt += "<tr>" +
+        "<th scope=\"row\">" + row + "</th>" +
+        "<td id = \"t" + row + "\" + >" + "<a onclick = " + getUser(document.getElementById("t" + row + "").innerHTML) + ">" + obj.tweets.statuses[row - 1].user.name + "</a></td>" +
+        "<div class=\"tooltip\">" +
+        "<td>" + obj.tweets.statuses[x].text + "</td>" +
+        " <span class=\"tooltiptext\">Tooltip text</span></div>" +
+        "</tr>";
+      row++;
     }
-    else {
-        txt += "<tr>" +
-            "<th scope='row'></th>" +
-            "<td></td>" +
-            "<td>No tweets on this users profile have been flagged</td>" +
-            "</tr>"
-    }
+  } else {
+    txt += "<tr>" +
+      "<th scope=\"row\"></th>" +
+      "<td></td>" +
+      "<td>No tweets on this users profile have been flagged</td>" +
+      "</tr>";
+  }
 
-    //popuulate table and display
-    document.getElementById("kbody").innerHTML = txt;
-    document.getElementById("keyResults").style.display = 'block';
-
+  // populate table and display
+  document.getElementById("kbody").innerHTML = txt;
+  document.getElementById("keyResults").style.display = "block";
 }
