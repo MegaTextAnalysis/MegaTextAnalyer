ba6b4cc83fb5c1a88a98fcac58d17c0561ef6480
stephenMoran <morans8@tcd.ie>
2018-03-18 18:12:20 +0000

    frontend

--- public/homePage.js
+++ public/homePage.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   showhide();
    document.getElementById("back-but").style.display = 'none';
    document.getElementById("key-back-but").style.display = 'none';
+    document.getElementById("key-back-but").style.display = 'none';
+       document.getElementById("stat-but").style.display = 'none';
 
   //When user clicks search
   $("#btnSubmit").click(function() {
