41cde7463b8f9c0e8d2197fbf2ffc70ccdf5ee2e
simong21 <35733539+simong21@users.noreply.github.com>
2018-03-18 13:56:55 +0000

    Add files via upload

--- flags.js
+++ flags.js
@@ -1,21 +1,24 @@
 // let keywords={
-// "ISIS":5,
-// "ISIL":5,
-// "Caliphate":5,
-// "Khalifah":5,
-// "Tagut":20,
-// "Jizya":10,
-// "Mushrikeen":10,
-// "Dar al-Harb":10,
-// "Shirk":10,
-// "Allahu Akbar":20
+// "ISIS":2,
+// "ISIL":2,
+// "Caliphate":2,
+// "Khalifah":2,
+// "Tagut":10,
+// "Jizya":5,
+// "Khilafa":5,
+// "Mushrikeen":5,
+// "Dar al-Harb":5,
+// "Shirk":5,
+// "Allahu Akbar":10,
+// "Rafidah":5,
+// "Kufar":5
 // };
 
-let keywords = {
-  "Ireland": 5,
-  "Women": 10,
+module.exports = {
+  "Ireland": 2,
+  "#TeamOfUs": 2,
+  "Grand Slam": 5,
   "Irish": 5,
-  "6 Nations": 10,
-  "#IREvSCO": 10
+  "6 Nations": 5,
+  "#ENGvIRE": 5
 };
-exports.keywords = keywords;
