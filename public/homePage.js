2e07d98e7ee6aebeedd3c656c97931b83355c674
stephenMoran <morans8@tcd.ie>
2018-03-26 11:53:44 +0100

    frontend

--- public/homePage.js
+++ public/homePage.js
@@ -252,7 +252,8 @@ function drawChart(obj) {
   for (let x in obj.flagged) {
     var flag = obj.flagged[count - 2].flags[0];
     var threat = obj.flagged[count - 2].threatLevel;
-    flagsEntry = [count - 1 + ". " + flag, "Flags", threat, 20 - threat];
+    threat = threat + Math.floor((Math.random() * 10) + 1);
+    flagsEntry = [count - 1 + ". " + flag, "Flags", threat, threat];
     flags[count] = flagsEntry;
     count++;
   }
